29c20bb334e8be4a0d678bd3613020e4ce434103
Johannes Schneider <johannes.schneider@htwg-konstanz.de>
2025-05-15 16:37:05 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,13 @@
+# Namensgenerator
+
+Diese kleine Webapp generiert zufällige Namen, bestehend aus einem geschlechtsneutralen Vornamen und einem in Deutschland typischen Nachnamen, und kopiert den generierten Namen in die Zwischenablage.
+
+## Verwendung
+
+1. Öffnen Sie die Webseite.
+2. Klicken Sie auf den Button "Generate".
+3. Der generierte Name wird angezeigt und automatisch in die Zwischenablage kopiert.
+
+## Lizenz
+
+Dieses Projekt steht unter der MIT-Lizenz. Siehe die Datei `LICENSE` für weitere Details.
---
+++ script.js
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2025 Johannes Schneider
+ * Licensed under the MIT License.
+ */
+
+const firstNames = [
+    "Alex",
+    "Chris",
+    "Flo",
+    "Uli",
+    "Yves",
+    "Maxi",
+    "Toni",
+    "Kim",
+    "Sam",
+    "Sascha",
+    "Jamie",
+    "Robin",
+    "Charlie",
+    "Andrea",
+    "Dominique",
+    "Nicola"
+];
+
+const lastNames = [
+  "Schwarz",
+  "Weiss",
+  "Klein",
+  "Gross",
+  "Winter",
+  "Sommer",
+  "Vogel",
+  "Fuchs",
+  "Wald",
+  "Berg",
+  "Stein",
+  "Wolff",
+  "Engel",
+  "Herbst",
+  "Frühling",
+  "Blume",
+  "Rose",
+  "Stern",
+  "Himmel",
+  "Bach",
+  "Seidel",
+  "Brand",
+  "Kühn",
+  "Treu",
+  "Lind",
+  "Birke",
+  "Feld",
+  "Heide",
+  "Rain",
+  "Sonne",
+  "Glanz",
+  "Sand",
+  "Holz",
+  "Wiese",
+  "Loos",
+  "Grün",
+  "Bernstein",
+  "Rose"
+];
+
+function getRandomName() {
+    const firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+    const lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+    return `${firstName} ${lastName}`;
+}
+
+document.getElementById('generate-btn').addEventListener('click', () => {
+    const nameDisplay = document.getElementById('name-display');
+    nameDisplay.textContent = getRandomName();
+    navigator.clipboard.writeText(nameDisplay.textContent).then(() => {
+        console.log('Name copied to clipboard!');
+    }).catch(err => {
+        console.error('Failed to copy name: ', err);
+    });
+});
+
+const infoIcon = document.getElementById('info-icon');
+const infoBox = document.getElementById('info-box');
+infoIcon.addEventListener('click', () => {
+    infoBox.classList.toggle('visible');
+});
